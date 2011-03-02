54f153ec91545a92c7d2d432dffb94278aab0060
Vincent Vinet <vvinet@revolutionlinux.com>
2011-03-01 19:59:05 -0500

    restore default sqlite database in django settings

--- etc/papas/settings.py
+++ etc/papas/settings.py
@@ -17,10 +17,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__),"..", "..", 'apmanager.sqlite')             # Or path to database file if using sqlite3.
-DATABASE_USER = 'apmanager'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'ExGldsaj3rt0tZQU'         # Not used with sqlite3.
-DATABASE_HOST = '10.145.3.200'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_NAME = 'papas.sqlite'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
